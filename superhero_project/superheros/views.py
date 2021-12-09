9ff102fbe8716b29558471cb03bc1f8e7578a5cb
JaredMartin0351 <jaredmartin0351@gmail.com>
2021-12-09 16:13:18 -0600

    Update views.py
    added to index method functionality to import all superhero objects from the superhero class

--- superhero_project/superheros/views.py
+++ superhero_project/superheros/views.py
@@ -1,6 +1,8 @@
+from .models import Superhero
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    all_heros = Superhero.objects.all()
     return render(request, 'superheros/index.html')
