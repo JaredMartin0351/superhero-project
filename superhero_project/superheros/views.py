fada26532e099541c0cdd3b399ab14a7fe49e820
JaredMartin0351 <jaredmartin0351@gmail.com>
2021-12-10 11:05:31 -0600

    Update views.py
    added to method to get single hero from the database using the primary key

--- superhero_project/superheros/views.py
+++ superhero_project/superheros/views.py
@@ -12,3 +12,7 @@ def index(request):
 
 def detail(request, hero_id):
     single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheros/detail.html', context)
